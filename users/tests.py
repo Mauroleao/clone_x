9e547e72093844444d8a56f5a1316201b7036b9c
Mauro <mauro_leao@hotmail.com>
2026-04-15 08:36:50 -0300

    primeiro teste criado

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class UserRegistrationTests(APITestCase):
+    def test_register_new_user(self):
+        """
+        Testa se a API permite a criação de um novo usuário
+        enviando username, email e password.
+        """
+        url = '/api/users/register/' 
+        
+        data = {
+            'username': 'novousuario',
+            'email': 'novo@email.com',
+            'password': 'senhaforte123'
+        }
+        
+        response = self.client.post(url, data)
+        
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        User = get_user_model()
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'novousuario')
--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UseSerializer
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UseSerializer
+    permission_classes = (AllowAny,)
+
+
